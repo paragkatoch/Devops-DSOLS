e44ae6d35977409d748fa3b4b93fbf21f6ede4dc
Parag Katoch <paragkatoch5@gmail.com>
2026-04-08 09:54:12 +0530

    switched to pool for multiple connections

--- server/internal/storage/postgres/postgres.go
+++ server/internal/storage/postgres/postgres.go
@@ -4,24 +4,24 @@ import (
 	"context"
 	"log/slog"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	config "github.com/paragkatoch/Devops-DSOLS/internal"
 	errhandler "github.com/paragkatoch/Devops-DSOLS/util/errHandler"
 )
 
 type Postgres struct {
-	Db *pgx.Conn
+	Db *pgxpool.Pool
 }
 
 func New(cfg *config.Config) *Postgres {
 	slog.Info("[+] Connecting with the DB")
 
-	conn, err := pgx.Connect(context.Background(),
+	pool, err := pgxpool.New(context.Background(),
 		cfg.Storage_path,
 	)
 	errhandler.FailOnError(err, "Unable to connect ot DB")
 
 	return &Postgres{
-		Db: conn,
+		Db: pool,
 	}
 }
--- server/util/DB/setupDB.go
+++ server/util/DB/setupDB.go
@@ -4,15 +4,15 @@ import (
 	"context"
 	"log/slog"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/paragkatoch/Devops-DSOLS/internal/storage/postgres"
 )
 
-func Init(conn *pgx.Conn) {
+func Init(st *postgres.Postgres) {
 	ctx := context.Background()
 
 	slog.Info("[+] Creating tables if not already exists")
 
-	conn.Exec(ctx, `CREATE TABLE IF NOT EXISTS orders (
+	st.Db.Exec(ctx, `CREATE TABLE IF NOT EXISTS orders (
 		id TEXT PRIMARY KEY,
 		user_id TEXT NOT NULL,
 		total_amount BIGINT NOT NULL,
@@ -22,7 +22,7 @@ func Init(conn *pgx.Conn) {
 		updated_at TIMESTAMP
 	);`)
 
-	conn.Exec(ctx, `CREATE TABLE IF NOT EXISTS order_items (
+	st.Db.Exec(ctx, `CREATE TABLE IF NOT EXISTS order_items (
 		id SERIAL PRIMARY KEY,
 		order_id TEXT REFERENCES orders(id) ON DELETE CASCADE,
 		product_id TEXT NOT NULL,
@@ -30,14 +30,14 @@ func Init(conn *pgx.Conn) {
 		price BIGINT NOT NULL
 	);`)
 
-	conn.Exec(ctx, `CREATE TABLE IF NOT EXISTS products (
+	st.Db.Exec(ctx, `CREATE TABLE IF NOT EXISTS products (
 		id TEXT PRIMARY KEY,
 		name TEXT NOT NULL,
 		price INT NOT NULL,
 		quantity INT NOT NULL
 	);`)
 
-	conn.Exec(ctx, `CREATE TABLE IF NOT EXISTS users (
+	st.Db.Exec(ctx, `CREATE TABLE IF NOT EXISTS users (
 		id TEXT PRIMARY KEY,
 		email text NOT NULL
 	);`)
