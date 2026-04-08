e2de4034dfcea1c7083eb6b7b24f11893e4f2bcb
Parag Katoch <paragkatoch5@gmail.com>
2026-04-08 21:09:02 +0530

    made rabbitmq sendMessage async using go routines

--- server/internal/rabbitmq/rabbitmq.go
+++ server/internal/rabbitmq/rabbitmq.go
@@ -52,29 +52,34 @@ func Connect(ch *amqp.Channel, channel string) amqp.Queue {
 }
 
 func SendMessage(ch *amqp.Channel, q amqp.Queue, body interface{}) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
 
-	jsonBody, err := json.Marshal(body)
-	if err != nil {
-		return err
-	}
+		jsonBody, err := json.Marshal(body)
+		if err != nil {
+			log.Println("failed to publish:", err)
+			return
+		}
 
-	err = ch.PublishWithContext(ctx,
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "application/json",
-			Body:        jsonBody,
-		})
+		err = ch.PublishWithContext(ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "application/json",
+				Body:        jsonBody,
+			})
 
-	if err != nil {
-		return err
-	}
+		if err != nil {
+			log.Println("failed to publish:", err)
+			return
+		}
 
-	log.Println(" [+] Sent a message")
+		// log.Println(" [+] Sent a message")
+	}()
+
 	return nil
 }
 
@@ -95,7 +100,7 @@ func ReceiveMessage(ch *amqp.Channel, q amqp.Queue, handler func([]byte)) {
 		log.Println(" [*] Waiting for messages.")
 
 		for d := range msgs {
-			log.Println(" [-] Received a message")
+			// log.Println(" [-] Received a message")
 			handler(d.Body)
 		}
 	})
